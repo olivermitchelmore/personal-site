4eaaa2879b8363865e78f2d7258eeb29e15c61e0
Ollie <superoli8000@gmail.com>
2026-02-11 12:50:00 +0000

    feat: add initial thought board logic

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,17 @@
 function add_thought() {
     console.log("button pressed");
-    let thought = document.getElementById("user_thought");
-    console.log(thought.value);
+    let thought_text = document.getElementById("user_thought");
+    
+    let thought = document.createElement('div');
+    thought.textContent = thought_text.value;
+
+    let board = document.getElementById("thought_board");
+    board.appendChild(thought);
+    handle_thought_placement(thought);
+    console.log(thought_text.value);
+    // begin drag
+}
+
+function handle_thought_placement(thought) {
+    thought.addEventListener
 }
